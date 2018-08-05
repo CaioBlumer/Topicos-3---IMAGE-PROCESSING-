662a0ca82410726df82683715b725aa101fe43b1
CaioBlumer <caioblumer5@gmail.com>
2018-08-05 19:21:56 -0300

    Add files via upload

---
+++ Warmup 1/IOHeader.h
@@ -0,0 +1,90 @@
+#ifndef IOHEADER_H_INCLUDED
+#define IOHEADER_H_INCLUDED
+
+unsigned char* ReadPGM(const char* filename, unsigned int &W, unsigned int &H, char* type)
+{
+    FILE* fp = fopen(filename, "r");
+    unsigned int maxVal=0;
+    unsigned char* data;
+
+    if(!fp)
+        return NULL;
+    fscanf(fp, "%c", &type[0]);
+    fscanf(fp, "%c", &type[1]);
+    if(type[0] == 'P' && type[1] == '2')
+    {
+        fscanf(fp, "%d %d %d", &W, &H, &maxVal);
+        data = (unsigned char*) calloc(W*H, sizeof(unsigned int));
+
+        for(int i=0; i<W*H; i++)
+        {
+            fscanf(fp, "%d ", &data[i]);
+        }
+    }
+    if(type[0] == 'P' && type[1] == '3')
+    {
+        fscanf(fp, "%d %d %d", &W, &H, &maxVal);
+        data = (unsigned char*) calloc(W*H*3, sizeof(unsigned int));
+
+        for(int i=0; i<W*H*3; i++)
+        {
+            fscanf(fp, "%d ", &data[i]);
+        }
+    }
+
+    fclose(fp);
+    return data;
+}
+
+bool SavePGM(const char* filename, unsigned char *data, unsigned int W, unsigned int H, char* type)
+{
+    FILE* fp = fopen(filename, "w");
+    if(!fp)
+        return false;
+    if(type[1] == '2')
+    {
+        fprintf(fp, "P2\n%d %d 255\n", W, H);
+        for(int i=0; i<W*H; i++)
+        {
+            fprintf(fp, "%d ", data[i]);
+        }
+    }
+    if(type[1] == '3')
+    {
+        fprintf(fp, "P3\n%d %d 255\n", W, H);
+        for(int i=0; i<W*H*3; i++)
+        {
+            fprintf(fp, "%d ", data[i]);
+        }
+    }
+    fclose(fp);
+    return data;
+}
+
+void inverted(unsigned char *data, unsigned int W, unsigned int H, char* type)
+{
+    unsigned int index;
+    if(type[1] == '2')
+    {
+        for(int i=0; i<H; i++)
+            for(int j=0; j<W; j++)
+            {
+                index = i*W + j;
+                data[index] = 255 - data[index];
+            }
+    }
+    if(type[1] == '3')
+    {
+        for(int i=0; i<H*3; i++)
+            for(int j=0; j<W; j++)
+            {
+                for(int k=0; k<3; k++)
+                {
+                    index = i*W + j + k;
+                    data[index] = 255 - data[index];
+                }
+            }
+    }
+}
+
+#endif // IOHEADER_H_INCLUDED
---
+++ Warmup 1/Warmup filtro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "IOHeader.h"
+using namespace std;
+char type[2];
+int main(int argc, char* argv[1])
+{
+
+    unsigned int H, W;
+    H=W=0;
+    unsigned char *data = ReadPGM(argv[1], W, H, type);
+    if(argc != 2)
+    {
+        printf("Input arguments error. Input should be <executable file.pgm>\n");
+        return 1;
+    }
+
+    inverted(data, W, H, type);
+
+    SavePGM("NewPGM.pgm", data, W, H, type);
+
+    free(data);
+    return 0;
+}
+
